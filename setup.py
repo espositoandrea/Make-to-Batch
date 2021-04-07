ffe29a96d5260c2484eedd788fe9285df11a74a1
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-07 09:45:59 +0200

    refatctor: Remove comment

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
         'Topic :: Utilities',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
